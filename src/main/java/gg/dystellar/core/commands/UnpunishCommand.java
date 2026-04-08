a7f8197b72b5cbcfb44ca39bd5d8d4483d3f6f6c
Algorhythmics <intivioli@gmail.com>
2026-04-08 23:50:16 +0200

    Updates

--- src/main/java/gg/dystellar/core/commands/UnpunishCommand.java
+++ src/main/java/gg/dystellar/core/commands/UnpunishCommand.java
@@ -1,96 +1,44 @@
 package gg.dystellar.core.commands;
 
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.awt.Color;
+
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.NameMatching;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
+import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
+import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.universe.Universe;
+
+import gg.dystellar.core.common.UserComponent;
+import gg.dystellar.core.utils.Utils;
 
 /**
  * This command lets admins remove punishments from a player.
  */
-public class UnpunishCommand implements Listener, CommandExecutor {
-
-    public static final ConcurrentMap<UUID, Map.Entry<UUID, Punishment[]>> invs = new ConcurrentHashMap<>();
+public class UnpunishCommand extends CommandBase {
+	private final RequiredArg<String> nameArg = this.withRequiredArg("player", "The player to unpunish", ArgTypes.STRING);
+	private final RequiredArg<Integer> idArg = this.withRequiredArg("id", "Punishment id, see with /punishments <player>", ArgTypes.INTEGER);
 
     public UnpunishCommand() {
-        Bukkit.getPluginCommand("unpunish").setExecutor(this);
-        Bukkit.getPluginManager().registerEvents(this, DystellarCore.getInstance());
+		super("unpunish", "Remove a punishment from a player");
+		this.requirePermission("dystellar.unpunish");
     }
 
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!(commandSender instanceof Player))
-			return true;
+	@Override
+	protected void executeSync(CommandContext ctx) {
+		final var name = ctx.get(nameArg);
+		final var id = ctx.get(idArg);
+		final var p = Universe.get().getPlayer(name, NameMatching.EXACT_IGNORE_CASE);
 
-        Player p = (Player) commandSender;
-
-        if (strings.length < 1) {
-            p.sendMessage(ChatColor.RED + "Error: You must specify a player!");
-            p.sendMessage(ChatColor.RED + "Usage: /unpunish <player>");
-            return true;
-        }
-
-        Player pInt = Bukkit.getPlayer(strings[0]);
-
-        if (pInt != null && pInt.isOnline()) {
-            invs.put(p.getUniqueId(), new AbstractMap.SimpleImmutableEntry<>(pInt.getUniqueId(), new Punishment[27]));
-            Inventory inv = InventoryBuilder.punishmentsInv(p, User.get(pInt).getPunishments());
-            p.openInventory(inv);
-        } else {
-            p.sendMessage(ChatColor.DARK_AQUA + "This player is not online in your server. Asking data to the proxy...");
-            p.sendMessage(ChatColor.DARK_AQUA + "Please wait...");
-            DystellarCore.getInstance().sendPluginMessage(p, DEMAND_PUNISHMENTS_DATA, strings[0]);
-        }
-
-        return true;
-    }
-
-    @EventHandler
-    public void onClick(InventoryClickEvent event) {
-        if (!event.getInventory().getTitle().equals(TITLE) || event.getCurrentItem() == null)
-			return;
-		event.setCancelled(true);
-
-        Punishment pun = invs.get(event.getWhoClicked().getUniqueId()).getValue()[event.getSlot()];
-
-        if (pun == null)
-			return;
-
-        Player pInt = Bukkit.getPlayer(invs.get(event.getWhoClicked().getUniqueId()).getKey());
-        Player p = (Player) event.getWhoClicked();
-
-        if (pInt != null && pInt.isOnline()) {
-            User user = User.get(pInt);
-
-            user.getPunishments().remove(pun);
-            event.getWhoClicked().closeInventory();
-            p.sendMessage(ChatColor.GREEN + "Punishment removed!");
-            pInt.sendMessage(ChatColor.GREEN + "The punishment with ID " + pun.hashCode() + " was removed from your punishments list!");
-
-            String[] details = new String[] {
-                    ChatColor.DARK_GREEN + "Punishment details:",
-                    "===============================",
-                    ChatColor.DARK_AQUA + "Type" + ChatColor.WHITE + ": " + ChatColor.GRAY + p.getClass().getSimpleName(),
-                    ChatColor.DARK_AQUA + "Creation Date" + ChatColor.WHITE + ": " + ChatColor.GRAY + pun.getCreationDate().format(DateTimeFormatter.ISO_DATE_TIME),
-                    ChatColor.DARK_AQUA + "Expiration Date" + ChatColor.WHITE + ": " + ChatColor.GRAY + (pun.getExpirationDate() == null ? "Never" : pun.getExpirationDate().format(DateTimeFormatter.ISO_DATE_TIME)),
-                    ChatColor.DARK_AQUA + "Reason" + ChatColor.WHITE + ": " + ChatColor.GRAY + pun.getReason(),
-                    "==============================="
-            };
-
-            pInt.sendMessage(details);
-        } else {
-            p.sendMessage(ChatColor.GREEN + "Sending request...");
-            DystellarCore.getInstance().sendPluginMessage(p, DystellarCore.REMOVE_PUNISHMENT_BY_ID, invs.get(p.getUniqueId()).getKey().toString(), invs.get(p.getUniqueId()).getValue()[event.getSlot()].hashCode());
-        }
-    }
-
-    @EventHandler
-    public void onClose(InventoryCloseEvent event) {
-        if (event.getInventory().getTitle().equals(TITLE)) invs.remove(event.getPlayer().getUniqueId());
-    }
-
-    @EventHandler
-    public void onDrag(InventoryDragEvent event) {
-        if (event.getInventory().getTitle().equals(TITLE)) event.setCancelled(true);
-    }
+		if (p != null && p.isValid()) {
+			final var user = p.getHolder().getComponent(UserComponent.getComponentType());
+			if (Utils.removeFirst(user.punishments, pun -> pun.getId() == id).isPresent())
+				ctx.sender().sendMessage(Message.raw("Punishment removed!").color(Color.GREEN));
+			else
+				ctx.sender().sendMessage(Message.raw("No such punishment exists with the provided id").color(Color.RED));
+		} else {
+			// TODO
+		}
+	}
 }
