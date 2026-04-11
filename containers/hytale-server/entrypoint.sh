de8c877422645c7922b89995b539409b69fe43d0
Algorhythmics <intivioli@gmail.com>
2026-04-11 13:56:46 +0200

    Setup docker containers

--- containers/hytale-server/entrypoint.sh
+++ containers/hytale-server/entrypoint.sh
@@ -2,7 +2,29 @@
 
 cd "$BASEDIR"
 
-latest=$(ls ../hytale-release/*.zip | sort -t'-' -k1 | tail -1)
+mkdir -p Server
+mkdir -p Server/mods
+cp "$JAR_PLUGIN_PATH" Server/mods/DystellarCore.jar
+mkdir Server/mods/gg.dystellar_Core
 
-unzip -o "../hytale-release/$latest" -d .
+cat > Server/mods/gg.dystellar_Core/setup.json << EOF
+{
+	"debug_mode": true,
+	"server_name": "$SERVER_NAME",
+	"scoreboard": true,
+	"allow_banned_players": false,
+	"resourcepack": false,
+	"automated_messages": true,
+	"automated_messages_rate": 240,
+	"prevent_weather": true,
+	"forced_weather": "Zone1_Sunny",
+	"public_ip": "localhost",
+	"api": "http://$BACKEND",
+	"websocket_endpoint": "ws://$BACKEND/api/core/create_ws",
+	"api_key": "$API_KEY"
+}
+EOF
+cd Server
+cp ${AUTH_ENCRYPT_PATH} auth.enc
 
+exec java -Xmx$RAM_ALLOC -jar HytaleServer.jar --assets ../Assets.zip
